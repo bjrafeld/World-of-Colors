d2f932246f33e8d000be8e6274a4b8bc9f277a71
Brendan Rafeld <bjrafeld@gmail.com>
2014-04-28 13:27:26 -0700

    Added prefabs and mechanics for primary world

---
+++ ColorPlatformer2/Assets/Scripts/TriggerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSpawner : MonoBehaviour {
+	
+	//Script for the Spawner Empty Object
+	
+	public GameObject weightPrefab;
+	
+	private GameObject weight;
+	
+	public bool spawnOnStart = true;
+	
+	
+	// Use this for initialization
+	void Start () {
+		if(spawnOnStart) {
+			spawnWeight();
+		}
+	}
+	
+	// Update is called once per frame
+	
+	public void spawnWeight() {
+		weight = Instantiate(weightPrefab, transform.position, transform.rotation) as GameObject;
+	}
+}
