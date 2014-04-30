9e076f220a14780de6c2676b16484a6d9398459a
bjrafeld <bjrafeld@gmail.com>
2014-04-30 15:22:59 -0700

    Added Spike Code

---
+++ ColorPlatformer2/Assets/Scripts/SpikeKill.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeKill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "Player") {
+			col.gameObject.collider2D.enabled = false;
+		}
+	}
+}
