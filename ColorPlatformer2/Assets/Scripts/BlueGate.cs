b14de4db17e3a5dd69f766e32febad066c65c8c9
bjrafeld <bjrafeld@gmail.com>
2014-04-29 17:41:01 -0700

    Added prefab placeholders for neon world

---
+++ ColorPlatformer2/Assets/Scripts/BlueGate.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueGate : MonoBehaviour {
+
+	public enum Direction {Up, Down, Left, Right};
+
+	public Direction movementDirection = Direction.Down;
+	public float movementAmount;
+	public float movementSpeed;
+
+	public GameObject affectedObject;
+	public Direction affectedDirection;
+	public float affectedMovementAmount;
+	public float affectedMovementSpeed;
+
+	private bool triggered = false;
+
+	private Vector3 _bluePosition;
+	private Vector3 _affectedPosition;
+
+	// Use this for initialization
+	void Awake () {
+		_bluePosition = this.transform.position;
+		_affectedPosition = affectedObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggered) {
+			MoveSelf();
+			MoveAffected();
+		} else {
+			ReturnSelf();
+			ReturnAffected();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag == "weight") {
+			triggered = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col) {
+		if(col.gameObject.tag == "weight") {
+			triggered = false;
+		}
+	}
+
+	public void TriggerOff() {
+		triggered = false;
+	}
+
+	private void MoveSelf() {
+		//Need to change if direction is not down
+		if(this.transform.position.y > (_bluePosition.y - movementAmount)) {
+			this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y) - (movementSpeed*Time.deltaTime));
+		} else if (this.transform.position.y < (_bluePosition.y - movementAmount)) {
+			this.transform.position = new Vector3(this.transform.position.x, _bluePosition.y - movementAmount);
+		}
+	}
+
+	private void MoveAffected() {
+		if (affectedDirection == Direction.Down) {
+			if (affectedObject.transform.position.y > (_affectedPosition.y - affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, (affectedObject.transform.position.y) - (affectedMovementSpeed*Time.deltaTime));
+			} else if (affectedObject.transform.position.y < (_affectedPosition.y - movementAmount)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, _affectedPosition.y - affectedMovementAmount);
+			}
+		} else if (affectedDirection == Direction.Up) {
+			if (affectedObject.transform.position.y < (_affectedPosition.y + affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, (affectedObject.transform.position.y) + (affectedMovementSpeed*Time.deltaTime));
+			} else if (affectedObject.transform.position.y > (_affectedPosition.y + affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, _affectedPosition.y + affectedMovementAmount);
+			}
+		} else if (affectedDirection == Direction.Left) {
+			if (affectedObject.transform.position.x > (_affectedPosition.x - affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3((affectedObject.transform.position.x) - (affectedMovementSpeed*Time.deltaTime), affectedObject.transform.position.y);
+			} else if (affectedObject.transform.position.x < (_affectedPosition.x - movementAmount)) {
+				affectedObject.transform.position = new Vector3(_affectedPosition.x - affectedMovementAmount, affectedObject.transform.position.y);
+			}
+		} else if (affectedDirection == Direction.Right) {
+			if (affectedObject.transform.position.x < (_affectedPosition.x + affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3((affectedObject.transform.position.x) + (affectedMovementSpeed*Time.deltaTime), affectedObject.transform.position.y );
+			} else if (affectedObject.transform.position.x > (_affectedPosition.x + affectedMovementAmount)) {
+				affectedObject.transform.position = new Vector3(_affectedPosition.x + affectedMovementAmount, affectedObject.transform.position.y);
+			}
+		}
+	}
+
+	private void ReturnSelf() {
+		if(this.transform.position.y <_bluePosition.y) {
+			this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y) + (movementSpeed*Time.deltaTime));
+		} else if (this.transform.position.y > _bluePosition.y) {
+			this.transform.position = new Vector3(this.transform.position.x, _bluePosition.y);
+		}
+	}
+
+	private void ReturnAffected() {
+		if (affectedDirection == Direction.Up) {
+			if (affectedObject.transform.position.y > (_affectedPosition.y)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, (affectedObject.transform.position.y) - (affectedMovementSpeed*Time.deltaTime));
+			} else if (affectedObject.transform.position.y < (_affectedPosition.y)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, _affectedPosition.y);
+			}
+		} else if (affectedDirection == Direction.Down) {
+			if (affectedObject.transform.position.y < (_affectedPosition.y)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, (affectedObject.transform.position.y) + (affectedMovementSpeed*Time.deltaTime));
+			} else if (affectedObject.transform.position.y > (_affectedPosition.y)) {
+				affectedObject.transform.position = new Vector3(affectedObject.transform.position.x, _affectedPosition.y);
+			}
+		} else if (affectedDirection == Direction.Right) {
+			if (affectedObject.transform.position.x > (_affectedPosition.x)) {
+				affectedObject.transform.position = new Vector3((affectedObject.transform.position.x) - (affectedMovementSpeed*Time.deltaTime), affectedObject.transform.position.y);
+			} else if (affectedObject.transform.position.x < (_affectedPosition.x)) {
+				affectedObject.transform.position = new Vector3(_affectedPosition.x, affectedObject.transform.position.y);
+			}
+		} else if (affectedDirection == Direction.Left) {
+			if (affectedObject.transform.position.x < (_affectedPosition.x)) {
+				affectedObject.transform.position = new Vector3((affectedObject.transform.position.x) + (affectedMovementSpeed*Time.deltaTime), affectedObject.transform.position.y );
+			} else if (affectedObject.transform.position.x > (_affectedPosition.x)) {
+				affectedObject.transform.position = new Vector3(_affectedPosition.x, affectedObject.transform.position.y);
+			}
+		}
+	}
+}
---
+++ ColorPlatformer2/Assets/Scripts/FreezeOnRed.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeOnRed : MonoBehaviour {
+
+	private CharacterPhysics _player;
+
+	// Use this for initialization	
+	void Start () {
+		Debug.Log (GameObject.FindGameObjectsWithTag("Player"));
+		_player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterPhysics>() as CharacterPhysics;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		Debug.Log ("Collided with " + col.gameObject.tag);
+		if(col.gameObject.tag == "Player") {
+			_player.movementFrozen = true;
+		}
+	}
+}
