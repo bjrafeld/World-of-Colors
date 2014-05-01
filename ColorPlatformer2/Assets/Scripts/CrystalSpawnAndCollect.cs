94fa30b10630f78bc4e11d2aa6b6f26fc3ce725a
Brendan Rafeld <bjrafeld@gmail.com>
2014-05-01 16:46:11 -0700

    Added crystal collection and animation

--- ColorPlatformer2/Assets/Scripts/CrystalSpawnAndCollect.cs
+++ ColorPlatformer2/Assets/Scripts/CrystalSpawnAndCollect.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class CrystalSpawnAndCollect : MonoBehaviour {
 
+	public GameObject shardParticles;
+	public int particleCount = 20;
+	public float explosionForce;
+
 	// Use this for initialization
 	void Start () {
 		//Check if it's already been collected
@@ -14,6 +18,15 @@ public class CrystalSpawnAndCollect : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
-
+		//Save that's it has been collected
+		//Save the total count of crystals
+		for(int i = 0; i < particleCount; i++) {
+			Quaternion rotation = Random.rotation;
+			rotation.x = 0;
+			rotation.y = 0;
+			GameObject particle = Instantiate(shardParticles, this.transform.position, rotation) as GameObject;
+			particle.rigidbody2D.AddForce(new Vector3(Random.Range (-359, 359), Random.Range (-359, 359), 0) * explosionForce);
+		}
+		Destroy(this.gameObject);
 	}
 }
---
+++ ColorPlatformer2/Assets/Scripts/ParticlesReturn.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlesReturn : MonoBehaviour {
+
+	private GameObject _player;
+	public float speed = 1.5f;
+
+	public float lifetime = 4;
+
+	private bool firstHit = true;
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime;
+		if(lifetime <= 0) {
+			Destroy(this.gameObject);
+		}
+		Vector3 newVector = _player.transform.position - this.transform.position;
+		this.rigidbody2D.AddForce(newVector * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log ("triggerd");
+		if(col.tag == "Player") {
+			if(firstHit) {
+				firstHit = false;
+			} else {
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
